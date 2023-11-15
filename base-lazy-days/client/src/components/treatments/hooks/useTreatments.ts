099825c079c0e0f0f363c23f550f93108f1c3dcb
schatzen <gomieti@naver.com>
2023-11-16 00:09:53 +0900

    feat: 커스텀 훅 복습 (useStaff)

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -13,7 +12,6 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const toast = useCustomToast();
 
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
